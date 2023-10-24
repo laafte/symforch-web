60a9fb0cbb12ee28cf15c46760e6ae368b1c8846
Sigurd Almåsbakk Skogmo <sigurd.skogmo@gmail.com>
2023-10-24 22:54:03 +0200

    Add branch for feature "simpler concert-management"

---
+++ src/CONCERTS.js
@@ -0,0 +1,10 @@
+const CONCERTS = [
+    {
+        tilte: "Til lands, til vanns og i lufta med",
+        banner: "path_to_banner.jpg",
+        when: "28. oktober kl. 13:00",
+        tickets: "https://www.uka.no/program/858-symforch-presenterer/1222/",
+        conductor: "Espen Andersen",
+        description: "Bli med på en reise inn i filmverdenens magi med Studentersamfundets Symfoniorkester. Under ledelse av dirigent Espen Andersen tar Symforch deg med på en følelsesladet reise gjennom uforglemmelige filmlydspor. Fra hjertevarmende øyeblikk til episke scener, vil aftenen utvilsomt vekke minner og berøre ditt musikk- og filmhjerte. Bli med og del denne stemningsfulle opplevelsen med oss!"
+    }
+];
